657c262deb8083e5b55855da162f2a95e52e6fc8
Lucas Mori <lucastecnew@gmail.com>
2021-03-05 18:02:42 -0300

    :loud_sound: Add Logger routine

---
+++ server/logger/common/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
+const fs = require('fs');
+const path = require('path');
+
+function getLogger(directory) {
+  const logDir = path.resolve(process.env.NUTRIEASY_LOGDIR || process.env.NUTRIEASY_LOGDIR, directory);
+
+  if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
+
+  const logger = createLogger({
+    format: format.combine(
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      format.printf(info => {
+        info.label = info.label ? info.label + " " : "";
+        return `${info.timestamp} ${info.label}${info.level}: ${info.message}`;
+      })
+    ),
+    transports: [
+      new transports.DailyRotateFile({
+        filename: `${logDir}/${directory}-%DATE%-main.log`,
+        datePattern: 'YYYY-MM-DD',
+        level: "debug"
+      })
+    ]
+  })
+
+  return logger;
+}
+
+module.exports = getLogger;
