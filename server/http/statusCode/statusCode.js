b9e07f9c3f340eda1ca14fc4ed5fa56e9847cafe
Lucas Mori <lucastecnew@gmail.com>
2021-03-17 01:15:08 -0300

    :sparkles: Add app helpers

---
+++ server/http/statusCode/statusCode.js
@@ -0,0 +1,14 @@
+const StatusCode = {
+  OK_200: 200,
+  Created_201: 201,
+  No_Content_204: 204,
+
+  Bad_Request_400: 400,
+  Unauthorized_401: 401,
+  Forbidden_403: 403,
+  Not_Found_404: 404,
+
+  Internal_Server_Error_500: 500
+}
+
+module.exports = {StatusCode}
