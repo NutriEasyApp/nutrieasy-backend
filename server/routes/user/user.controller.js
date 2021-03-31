2b23fe56ba6a94cf4e658b890616148fe5383d9f
Davi Marcelino <davifmarcelino@gmail.com>
2021-03-31 19:40:48 -0300

    :sparkles: add route user

---
+++ server/routes/user/user.controller.js
@@ -0,0 +1,25 @@
+const { UserDao } = require('../../dao/user.dao');
+
+class UserController {
+  constructor() {
+    this.daoUser = new UserDao();
+  }
+
+  async getUser(request, response) {
+    try {
+      console.log(request.params.id);
+      const user = await this.daoUser.getUser(request.params);
+
+      if (!user)
+        return response
+          .status(404)
+          .json({ error: 404, message: 'The User was not found' });
+
+      return response.status(200).json(user);
+    } catch (err) {
+      return response.status(404).send({ error: 404, message: err.message });
+    }
+  }
+}
+
+module.exports = { UserController };
