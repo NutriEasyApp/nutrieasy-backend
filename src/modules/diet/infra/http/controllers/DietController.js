da1640892d7ce3f6fb0cb97b261a2c074f30721d
Davi Marcelino <davifmarcelino@gmail.com>
2021-05-30 17:47:11 -0300

    :bug: fix dietController response

--- src/modules/diet/infra/http/controllers/DietController.js
+++ src/modules/diet/infra/http/controllers/DietController.js
@@ -6,7 +6,7 @@ class DietController {
     const getDietService = new GetDietService();
     const diet = await getDietService.execute({ id_user });
 
-    return response.status(200).json({ diet, getMeals });
+    return response.status(200).json({ diet });
   }
 }
 
