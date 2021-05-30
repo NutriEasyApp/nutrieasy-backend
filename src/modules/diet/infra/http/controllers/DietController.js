a3ed9eb78d20cb769b187c9105edd8a05772ffb7
Lucas Mori <lucastecnew@gmail.com>
2021-05-30 14:26:39 -0300

    🐛 fix: remove try catch

--- src/modules/diet/infra/http/controllers/DietController.js
+++ src/modules/diet/infra/http/controllers/DietController.js
@@ -2,19 +2,19 @@ const { GetDietService } = require('../../../services/GetDietService');
 const { CreateMealDiet } = require('../../../providers/createMealDiet');
 class DietController {
   async show(request, response) {
-    try {
-      const { id_user } = request.params;
+    // try {
+    const { id_user } = request.params;
 
-      const getDietService = new GetDietService();
-      const diet = await getDietService.execute({ id_user });
-      const getMeals = new CreateMealDiet(diet.diet).getMeals();
+    const getDietService = new GetDietService();
+    const diet = await getDietService.execute({ id_user });
+    const getMeals = new CreateMealDiet(diet.diet).getMeals();
 
-      return response.status(200).json({ diet, getMeals });
-    } catch (err) {
-      return response
-        .status(err.statusCode || 404)
-        .json({ message: err.message });
-    }
+    return response.status(200).json({ diet, getMeals });
+    // } catch (err) {
+    // return response
+    // .status(err.statusCode || 404)
+    // .json({ message: err.message });
+    // }
   }
 }
 
