29ee54b628b750917656fed53d1ba1e68de7f426
Lucas Mori <lucastecnew@gmail.com>
2021-06-06 21:39:02 -0300

    ✨ feat: add notification

--- src/modules/users/services/ForgotPasswordService.js
+++ src/modules/users/services/ForgotPasswordService.js
@@ -10,7 +10,7 @@ class ForgotPasswordService {
 
   async execute({ email }) {
     if (!email) throw new AppError('Data was not provided', 400);
-    const userExists = await this.userRepository.getUserIdByEmail({ email });
+    const userExists = await this.userRepository.getUserByEmail({ email });
     if (!userExists) throw new AppError('User does not exists', 400);
 
     const code = Math.floor(100000 + Math.random() * 900000).toString();
@@ -19,7 +19,12 @@ class ForgotPasswordService {
       email,
       code,
     });
-
+    console.log(userExists.token);
+    await PushNotification({
+      pushNotificationTokens: [userExists.token],
+      title: `Aqui está seu código temporário`,
+      body: `Seu código temporário: ${code}.`,
+    });
     return { code };
   }
 }
